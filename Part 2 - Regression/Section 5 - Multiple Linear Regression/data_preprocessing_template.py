4e4d8d3dadc6a160bcccb478c07b9eebbf4fe8b6
Patryk <patrykdziedzioch@gmail.com>
2018-08-15 16:56:04 +0200

    Multiple linear regression - end

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/data_preprocessing_template.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/data_preprocessing_template.py
@@ -6,12 +6,27 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Data.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+dataset = pd.read_csv('50_Startups.csv')
+print(dataset.head())
+X = dataset.iloc[:, :-1].values #without last column
+y = dataset.iloc[:, 4].values
+
+print(y)
+
+# Encoding categorical data
+# Encoding the Independant Variable
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
+onehotencoder = OneHotEncoder(categorical_features= [3])
+X = onehotencoder.fit_transform(X).toarray()
+#print(X)
+
+# Avoiding the dummy variable trap
+X = X[:, 1:]
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
@@ -21,3 +36,51 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
+
+#Fitting Multiple Linear regression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predisting the Test set results
+y_pred = regressor.predict(X_test)
+
+# Building the optimal model using Backward Elimination
+import statsmodels.formula.api as sm
+X = np.append(arr=np.ones((50, 1)).astype(int), values=X, axis=1)
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+
+print(regressor_OLS.summary())
+
+X = np.append(arr=np.ones((50, 1)).astype(int), values=X, axis=1)
+X_opt = X[:, [0, 1, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+
+print(regressor_OLS.summary())
+
+X = np.append(arr=np.ones((50, 1)).astype(int), values=X, axis=1)
+X_opt = X[:, [0, 1, 4, 5]]
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+
+print(regressor_OLS.summary())
+
+X = np.append(arr=np.ones((50, 1)).astype(int), values=X, axis=1)
+X_opt = X[:, [0, 1, 5]]
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+
+print(regressor_OLS.summary())
+
+X = np.append(arr=np.ones((50, 1)).astype(int), values=X, axis=1)
+X_opt = X[:, [0, 1]]
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+
+print(regressor_OLS.summary())
+
+
+
+
+
+
+
+
