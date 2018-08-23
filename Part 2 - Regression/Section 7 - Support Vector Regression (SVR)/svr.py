16dad9624622d9e59e2e25d6a655a913d0a45b40
Patryk <patrykdziedzioch@gmail.com>
2018-08-23 12:12:27 +0200

    SVR done

--- Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/svr.py
+++ Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/svr.py
@@ -1,4 +1,4 @@
-# Regression Template
+# SVR
 
 # Importing the libraries
 import numpy as np
@@ -8,7 +8,7 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
-y = dataset.iloc[:, 2:3].values
+y = dataset.iloc[:, 2].values
 
 # Splitting the dataset into the Training set and Test set
 """from sklearn.cross_validation import train_test_split
@@ -21,15 +21,14 @@ sc_y = StandardScaler()
 X = sc_X.fit_transform(X)
 y = sc_y.fit_transform(y)
 
-# Fitting the SVR to the dataset
+# Fitting SVR to the dataset
 from sklearn.svm import SVR
-regressor = SVR(kernel='rbf')
+regressor = SVR(kernel = 'rbf')
 regressor.fit(X, y)
 
 # Predicting a new result
-y_pred = sc_y.inverse_transform(regressor.predict(sc_X.transform(np.array([[6.5]]))))
-
-print(y_pred)
+y_pred = regressor.predict(6.5)
+y_pred = sc_y.inverse_transform(y_pred)
 
 # Visualising the SVR results
 plt.scatter(X, y, color = 'red')
@@ -39,8 +38,8 @@ plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
 
-# Visualising the Regression results (for higher resolution and smoother curve)
-X_grid = np.arange(min(X), max(X), 0.1)
+# Visualising the SVR results (for higher resolution and smoother curve)
+X_grid = np.arange(min(X), max(X), 0.01) # choice of 0.01 instead of 0.1 step because the data is feature scaled
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
 plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
