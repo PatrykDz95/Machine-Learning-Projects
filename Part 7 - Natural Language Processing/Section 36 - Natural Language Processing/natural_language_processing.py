f213247f5659b6f94ac563e08ba72c57dfc55c47
Patryk <patrykdziedzioch@gmail.com>
2018-09-10 13:40:50 +0200

    81% accuracy

--- Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/natural_language_processing.py
+++ Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/natural_language_processing.py
@@ -14,13 +14,28 @@ import nltk
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
+stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', "you're", "you've", "you'll",
+             "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's",
+             'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves',
+             'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was',
+             'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an',
+             'the', 'and', 'but', 'if', 'or', 'because', 'as', 'while', 'of', 'at', 'by', 'for', 'with',
+             'about', 'against', 'between', 'into', 'through', 'during', 'above', 'below', 'to',
+             'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once',
+             'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'such', 'only', 'own',
+             'same', 'so', 'than', 'too','s',
+             't', 'can', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're',
+             've', 'y', 'ain', 'aren', "aren't", 'didn', "didn't", 'doesn', "doesn't", 'hadn',
+             "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn',
+             "mustn't", 'needn', "needn't", 'shan', "shan't", 'weren',
+             "weren't", 'won']
 corpus = []
-for i in range(0, 1000):
+for i in range(0, dataset['Review'].size):
     review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i]) # replacing all characters that are not a-zA-Z with a space(' ')
     review = review.lower()
     review = review.split()
-    ps = PorterStemmer()
-    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    ps = PorterStemmer() # stemming = transforming words like loving, loved to just love
+    review = [ps.stem(word) for word in review if not word in stopwords]
     review = ' '.join(review)
     corpus.append(review)
 
@@ -28,7 +43,7 @@ for i in range(0, 1000):
 from sklearn.feature_extraction.text import CountVectorizer
 cv = CountVectorizer(max_features = 1500)
 X = cv.fit_transform(corpus).toarray()
-y = dataset.iloc[:, 1].values
+y = dataset.iloc[:, 1].values # all rows from column 1
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
@@ -42,8 +57,12 @@ classifier.fit(X_train, y_train)
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
 
+
 # Making the Confusion Matrix
+from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test, y_pred)
+cm = classification_report(y_test, y_pred)
+c = confusion_matrix(y_test, y_pred)
 
 print(cm)
+print(c)
