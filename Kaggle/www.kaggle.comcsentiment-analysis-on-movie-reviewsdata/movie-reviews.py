a897c14ed0fe56bda1af6000492acbefe623d91a
Patryk <patrykdziedzioch@gmail.com>
2018-09-11 19:40:59 +0200

    NLP and Kaggle

--- Kaggle/www.kaggle.comcsentiment-analysis-on-movie-reviewsdata/movie-reviews.py
+++ Kaggle/www.kaggle.comcsentiment-analysis-on-movie-reviewsdata/movie-reviews.py
@@ -1,88 +1,138 @@
-import numpy as np  # linear algebra
-import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
-# Cleaning the texts
-import re # for cleaning text
-import nltk
+import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
-nltk.download('stopwords')
+import seaborn as sns
+import nltk
 from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
-from wordcloud import WordCloud,STOPWORDS
-from nltk.stem import WordNetLemmatizer
-
-# Importing the dataset
-test_dataset = pd.read_csv('test.tsv', delimiter = '\t', quoting = 3)
-train_dataset = pd.read_csv('train.tsv', delimiter = '\t', quoting = 3)
-word_count=pd.value_counts(train_dataset['Sentiment'].values, sort=False)
-print(train_dataset.head())
-Index = [1,2,3,4,5]
-plt.figure(figsize=(10,5))
-plt.bar(Index,word_count,color = 'blue')
-plt.xticks(Index,['negative','neutral','somewhat negative','somewhat positive','positive'],rotation=45)
-plt.ylabel('word_count')
-plt.xlabel('word')
-plt.title('Count of Moods')
-#plt.bar(Index,len(train_dataset['Sentiment']),color = 'blue')
-plt.show()
-# stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', "you're", "you've", "you'll",
-#              "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's",
-#              'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves',
-#              'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was',
-#              'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an',
-#              'the', 'and', 'but', 'if', 'or', 'because', 'as', 'while', 'of', 'at', 'by', 'for', 'with',
-#              'about', 'against', 'between', 'into', 'through', 'during', 'above', 'below', 'to',
-#              'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once',
-#              'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'such', 'only', 'own',
-#              'same', 'so', 'than', 'too','s',
-#              't', 'can', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're',
-#              've', 'y', 'ain', 'aren', "aren't", 'didn', "didn't", 'doesn', "doesn't", 'hadn',
-#              "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn',
-#              "mustn't", 'needn', "needn't", 'shan', "shan't", 'weren',
-#              "weren't", 'won']
-def  review_to_words(raw_review):
-    review =raw_review
-    review = re.sub('[^a-zA-Z]', ' ',review)
-    review = review.lower()
-    review = review.split()
-    lemmatizer = WordNetLemmatizer()
-    review = [lemmatizer.lemmatize(w) for w in review if not w in set(stopwords.words('english'))]
-    return (' '.join(review))
-
-corpus= []
-for i in range(0, 156060):
-    corpus.append(review_to_words(train_dataset['Phrase'][i]))
-
-#
-# wordcloud = WordCloud(stopwords=STOPWORDS,
-#                       background_color='black',
-#                       width=3000,
-#                       height=2500
-#                      ).generate(review)
-
-# Creating the Bag of Words model
 from sklearn.feature_extraction.text import CountVectorizer
-cv = CountVectorizer(max_features = 1500)
-X = cv.fit_transform(corpus).toarray()
-y = train_dataset.iloc[:, 1].values # all rows from column 1
 
-# Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+yelp = pd.read_csv('yelp.csv')
+yelp['text length'] = yelp['text'].apply(len)
+print(yelp.head())
 
-# Fitting Naive Bayes to the Training set
-from sklearn.naive_bayes import BernoulliNB
-classifier = BernoulliNB()
-classifier.fit(X_train, y_train)
+#g = sns.FacetGrid(data=yelp, col='stars')
+#g.map(plt.hist, 'text length', bins=50)
 
-# Predicting the Test set results
-y_pred = classifier.predict(X_test)
+#sns.boxplot(x='stars', y='text length', data=yelp)
+
+stars = yelp.groupby('stars').mean()
+print(stars.corr())
+
+yelp_class = yelp[(yelp['stars'] == 1) | (yelp['stars'] == 5)]
+yelp_class.shape
+
+X = yelp_class['text']
+y = yelp_class['stars']
+
+import string
 
 
-# Making the Confusion Matrix
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
-cm = classification_report(y_test, y_pred)
-c = confusion_matrix(y_test, y_pred)
+def text_process(text):
+    '''
+    Takes in a string of text, then performs the following:
+    1. Remove all punctuation
+    2. Remove all stopwords
+    3. Return the cleaned text as a list of words
+    '''
+    nopunc = [char for char in text if char not in string.punctuation]
+    nopunc = ''.join(nopunc)
 
-print(cm)
-print(c)
+    return [word for word in nopunc.split() if word.lower() not in stopwords.words('english')]
+
+
+sample_text = "Hey there! This is a sample review, which happens to contain punctuations."
+print(text_process(sample_text))
+review_25 = X[24]
+
+bow_transformer = CountVectorizer(analyzer=text_process).fit(X)
+print(review_25)
+plt.show()
+
+# import numpy as np  # linear algebra
+# import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+# # Cleaning the texts
+# import re # for cleaning text
+# import nltk
+# import matplotlib.pyplot as plt
+# nltk.download('stopwords')
+# from nltk.corpus import stopwords
+# from nltk.stem.porter import PorterStemmer
+# from wordcloud import WordCloud,STOPWORDS
+# from nltk.stem import WordNetLemmatizer
+#
+# # Importing the dataset
+# test_dataset = pd.read_csv('test.tsv', delimiter = '\t', quoting = 3)
+# train_dataset = pd.read_csv('train.tsv', delimiter = '\t', quoting = 3)
+# word_count=pd.value_counts(train_dataset['Sentiment'].values, sort=False)
+# print(train_dataset.head())
+# Index = [1,2,3,4,5]
+# plt.figure(figsize=(10,5))
+# plt.bar(Index,word_count,color = 'blue')
+# plt.xticks(Index,['negative','neutral','somewhat negative','somewhat positive','positive'],rotation=45)
+# plt.ylabel('word_count')
+# plt.xlabel('word')
+# plt.title('Count of Moods')
+# #plt.bar(Index,len(train_dataset['Sentiment']),color = 'blue')
+# plt.show()
+# # stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', "you're", "you've", "you'll",
+# #              "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's",
+# #              'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves',
+# #              'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was',
+# #              'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an',
+# #              'the', 'and', 'but', 'if', 'or', 'because', 'as', 'while', 'of', 'at', 'by', 'for', 'with',
+# #              'about', 'against', 'between', 'into', 'through', 'during', 'above', 'below', 'to',
+# #              'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once',
+# #              'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'such', 'only', 'own',
+# #              'same', 'so', 'than', 'too','s',
+# #              't', 'can', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're',
+# #              've', 'y', 'ain', 'aren', "aren't", 'didn', "didn't", 'doesn', "doesn't", 'hadn',
+# #              "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn',
+# #              "mustn't", 'needn', "needn't", 'shan', "shan't", 'weren',
+# #              "weren't", 'won']
+# def  review_to_words(raw_review):
+#     review =raw_review
+#     review = re.sub('[^a-zA-Z]', ' ',review)
+#     review = review.lower()
+#     review = review.split()
+#     lemmatizer = WordNetLemmatizer()
+#     review = [lemmatizer.lemmatize(w) for w in review if not w in set(stopwords.words('english'))]
+#     return (' '.join(review))
+#
+# corpus= []
+# for i in range(0, 156060):
+#     corpus.append(review_to_words(train_dataset['Phrase'][i]))
+#
+# #
+# # wordcloud = WordCloud(stopwords=STOPWORDS,
+# #                       background_color='black',
+# #                       width=3000,
+# #                       height=2500
+# #                      ).generate(review)
+#
+# # Creating the Bag of Words model
+# from sklearn.feature_extraction.text import CountVectorizer
+# cv = CountVectorizer(max_features = 1500)
+# X = cv.fit_transform(corpus).toarray()
+# y = test_dataset.iloc[:, 1].values # all rows from column 1
+#
+# # Splitting the dataset into the Training set and Test set
+# from sklearn.cross_validation import train_test_split
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+#
+# # Fitting Naive Bayes to the Training set
+# from sklearn.naive_bayes import BernoulliNB
+# classifier = BernoulliNB()
+# classifier.fit(X_train, y_train)
+#
+# # Predicting the Test set results
+# y_pred = classifier.predict(X_test)
+#
+#
+# # Making the Confusion Matrix
+# from sklearn.metrics import classification_report
+# from sklearn.metrics import confusion_matrix
+# cm = classification_report(y_test, y_pred)
+# c = confusion_matrix(y_test, y_pred)
+#
+# print(cm)
+# print(c)
